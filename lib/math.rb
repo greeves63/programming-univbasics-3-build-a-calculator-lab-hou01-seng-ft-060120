1f1e5a3249c30c403c71fd298a909dfc7c628986
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-19 12:46:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,5 @@
 def addition(num1, num2)
-puts "Enter first value: "
-num1=gets.chomp.to_i
-puts "Enter second value: "
-num2=gets.chomp.to_i
-addition = num1 + num2
+  num1 + num2
 end
 
 def subtraction(num1, num2)
